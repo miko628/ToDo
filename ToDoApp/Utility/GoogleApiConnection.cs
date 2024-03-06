519dd4749f5b15365b82e79e3a2af1bab7fbc4fc
miko628 <96443763+miko628@users.noreply.github.com>
2024-03-06 23:21:40 +0100

    0.51 GoogleApi Insert + synchronize

--- ToDoApp/Utility/GoogleApiConnection.cs
+++ ToDoApp/Utility/GoogleApiConnection.cs
@@ -17,29 +17,34 @@ namespace ToDoApp.Utility
 {
     class GoogleApiConnection
     {
-       
-            private UserCredential credential;
-        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+
+        private UserCredential credential;
+        private CalendarService service;
+        static string[] Scopes = { CalendarService.Scope.Calendar, CalendarService.Scope.CalendarEvents, CalendarService.Scope.CalendarEventsReadonly };
         public GoogleApiConnection()
         {
             string CredentialsPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Resource\\credentials.json");
             using (var stream =
                        new FileStream(CredentialsPath, FileMode.Open, FileAccess.Read))
             {
-                string Path = "token.json";
+                string TokenPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Resource\\token.json");
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.FromStream(stream).Secrets,
                     Scopes,
                     "user",
                     CancellationToken.None,
-                    new FileDataStore(Path, true)).Result;
-                Trace.WriteLine("Credential file saved to: " + Path);
+                    new FileDataStore(TokenPath, true)).Result;
+                Trace.WriteLine("Credential file saved to: " + TokenPath);
             }
-            var service = new CalendarService(new BaseClientService.Initializer
+            service = new CalendarService(new BaseClientService.Initializer
             {
                 HttpClientInitializer = credential,
                 ApplicationName = "ToDoApp"
-            }) ;
+            });
+
+        }
+        public void GetAllTasks()
+        {
             EventsResource.ListRequest request = service.Events.List("primary");
             request.TimeMin = DateTime.Now;
             request.ShowDeleted = false;
@@ -65,6 +70,60 @@ namespace ToDoApp.Utility
                 Trace.WriteLine(eventItem.Summary, when);
             }
         }
-            
-}
+        public void PostTask(ToDoTask task)
+        {
+            Event body = new Event();
+            body.Summary = task.Name;
+            body.Description = task.Description;
+            EventDateTime start = new EventDateTime();
+            start.DateTime = task.TaskToDoDate;
+            //var end = start;
+            body.Start = start;
+            body.End = start;
+            EventsResource.InsertRequest request = new EventsResource.InsertRequest(service, body, "primary");
+            Event response = request.Execute();
+
+        }
+        public void SyncrhonizeTasks(List<ToDoTask> tasks)
+        {
+            List<Event> eventsToAdd = new List<Event>();
+            foreach (ToDoTask task in tasks)
+            {
+                Event body = new Event();
+                body.Summary = task.Name;
+                body.Description = task.Description;
+                EventDateTime start = new EventDateTime();
+                start.DateTime = task.TaskToDoDate;
+                body.Start = start;
+                body.End = start;
+                //eventsToAdd.Add(body);
+                Events existingEvents = service.Events.List("primary").Execute();
+                bool eventExists = false;
+                foreach (var existingEvent in existingEvents.Items)
+                {
+                    if (existingEvent.Summary == task.Name /*&& existingEvent.Start.DateTimeDateTimeOffset == task.TaskToDoDate */ && existingEvent.Description == task.Description)
+                    {
+                        eventExists = true;
+                        break;
+                    }
+                }
+
+                if (!eventExists)
+                {
+                    EventsResource.InsertRequest request = new EventsResource.InsertRequest(service, body, "primary");
+                    Event response = request.Execute();
+                    Trace.WriteLine("Dodano wydarzenie: " + response.Summary + " " + response.Description);
+                    Trace.WriteLine("Istniejacego wydarzenia: " + task.Name + " " + task.Description);
+                    //Trace.WriteLine("Dodano wydarzenie: " + response.Summary+" "+response.Start.DateTimeDateTimeOffset + " "+response.Description);
+                    //Trace.WriteLine("Istniejacego wydarzenia: " + task.Name + " " + task.TaskToDoDate + " " + task.Description);
+                }
+                else
+                {
+                    Trace.WriteLine("Wydarzenie już istnieje: " + task.Name);
+                }
+            }
+
+
+        }
+    }
 }
--- ToDoApp/ViewModel/CalendarViewModel.cs
+++ ToDoApp/ViewModel/CalendarViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Google.Protobuf.WellKnownTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,19 @@ using ToDoApp.Utility;
 
 namespace ToDoApp.ViewModel
 {
-    class CalendarViewModel: ViewModelBase
+    class CalendarViewModel : ViewModelBase
     {
+        public RelayCommand SynchronizeCommand { get; set; }
         public CalendarViewModel() 
         {
+            SynchronizeCommand = new RelayCommand(ExecuteSyncTasks, CanExecuteMyCommand);
+            //api.PostTask(new ToDoTask("nazwa", DateTime.Now.ToString(), DateTime.Now.ToString(), "","",null));
+        }
+        private void ExecuteSyncTasks ( object param)
+        {
+            List<ToDoTask> tasks = DbCrud.getCurrentTasks();
             GoogleApiConnection api = new GoogleApiConnection();
+            api.SyncrhonizeTasks(tasks);
         }
     }
 }
